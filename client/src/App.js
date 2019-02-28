5c80ba6e65eb2ecaf73dfdfa5f63c77af4d4a85c
Nahomy Puente <nahomypuentec@gmail.com>
2019-02-28 12:26:42 -0300

    Add CityList

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,62 @@
-import React, { Component } from 'react';
-import './App.css';
-import Provinces from './Provinces';
-import MapContainer from './MapContainer';
+import React, { Component } from "react";
+import MapContainer from "./MapContainer";
+
+import "./App.css";
+
+const data = [
+  {
+    name: "Buenos Aires",
+    title: "Buenos Aires",
+    lat: -34.603722,
+    lng: -58.381592,
+    cases_number: 25,
+    disease: "Dengue",
+    id: 1
+  }
+];
+
+const CityList = props => {
+  return (
+    <div>
+      <ul>
+        {props.items.map((item, index) => {
+          return (
+            <li key={index} onClick={e => props.onClick(e, item)}>
+              {item.title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 class App extends Component {
+  state = {
+    selectedItem: { 
+      lat: 0, 
+      lng: 0, 
+      boolselect: false
+    }
+  };
+
+  showInfo(e, selectedItem) {
+    selectedItem["boolselect"] = true;
+    this.setState({ selectedItem: selectedItem });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Aedes Zone</h1>
-          <Provinces/>
-          <MapContainer/>
-        </header>
+      <div>
+        <CityList items={data} onClick={this.showInfo.bind(this)} />
+        <MapContainer
+          center={{ lat: -34.603722, lng: -58.381592 }}
+          zoom={5}
+          data={data}
+          selectedItem={this.state.selectedItem}
+        />
       </div>
     );
   }
 }
-
 export default App;
