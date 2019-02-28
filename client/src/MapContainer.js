2ffd0a198e63decbd53ed644477e6e13e8954483
Nahomy Puente <nahomypuentec@gmail.com>
2019-02-28 00:56:55 -0300

    component map

--- client/src/MapContainer.js
+++ client/src/MapContainer.js
@@ -1,23 +1,41 @@
 import React, {Component} from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import './App.css';
+
 export class MapContainer extends Component {
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCDPo0EeLCKbnKRAk6MqzHMKnTzPhty2vs&callback=initMap")
+    window.initMap = this.initMap
+  }
+  initMap = () => {
+    // eslint-disable-next-line
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
     render() {
       return (
-        <Map google={this.props.google} zoom={14}>
-   
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-   
-          <InfoWindow onClose={this.onInfoWindowClose}>
-              <div>
-                  
-              </div>
-          </InfoWindow>
-        </Map>
+        <main>
+          <div id="map"></div>
+        </main>
       );
     }
   }
+
+  function loadScript(url) {
+    var index = window.document.getElementsByTagName("script")[0]
+    var script = window.document.createElement("script")
+    script.src = url
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
+  }
+  
+  
    
-  export default GoogleApiWrapper({
-    apiKey: ('AIzaSyAUYDtv51kgaUx_wEw3bfWkoSiYrXuKGlA')
-  })(MapContainer)
+  export default MapContainer;
